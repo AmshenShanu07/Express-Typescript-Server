58efc9d97d7d7e1976ef460a24fa4a154ab57e4f
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2023-03-18 20:36:39 +0530

    feat

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import * as http from 'http';
-import app from './src/app'
+import app from './src/app';
 
-const PORT:number | string = process.env.PORT || 4000;
+const PORT: number | string = process.env.PORT || 4000;
 const server = http.createServer(app);
 
-server.listen(PORT, ()=>{
-  console.log(`SERVER STARTED AT PORT ${PORT} 🚀`)
-})
+server.listen(PORT, () => {
+  console.log(`SERVER STARTED AT PORT ${PORT} 🚀🚀🚀`);
+});
